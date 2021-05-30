340d1d24421b83e746c1d0b7a1b9fec30eac19fd
ambarishg <ambarish.ganguly@gmail.com>
2021-05-30 11:34:06 +0530

    Added Clock function

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>;
+function Clock(props) {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {props.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
   }
   
-  const element = <Welcome name="Sara" />;
-  ReactDOM.render(
-    element,
-    document.getElementById('root')
-  );
+  function tick() {
+    ReactDOM.render(
+      <Clock date={new Date()} />,
+      document.getElementById('root')
+    );
+  }
+  
+  setInterval(tick, 1000);
